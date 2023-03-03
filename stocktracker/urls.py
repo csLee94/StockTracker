831e189fdd3f79883c0a574747d3e9fe5dd36e7d
csLee94 <lcs625@naver.com>
2023-03-04 00:10:34 +0900

    config app, urls, view and execute migration

---
+++ stocktracker/urls.py
@@ -0,0 +1,10 @@
+"""
+created at: 2023-03-04
+"""
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('', views.index),
+]
--- stocktracker/views.py
+++ stocktracker/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+"""
+created at: 2023-03-04
+"""
+
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    """
+    Index
+    """
+    return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
