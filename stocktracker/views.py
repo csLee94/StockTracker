e2bc08728decae223ab0f003ff5f8431a2fbf980
csLee94 <lcs625@naver.com>
2023-03-05 22:46:12 +0900

    update source about init asset_cash

--- stocktracker/views.py
+++ stocktracker/views.py
@@ -4,8 +4,8 @@ created at: 2023-03-04
 
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
-from .models import Account
-from .forms import AccountForm
+from .models import Account, AssetCash, AssetStock
+from .forms import AccountForm, AssetCashForm
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
@@ -40,8 +40,39 @@ def account_create(request):
             account = form.save(commit=False)
             account.user_id = request.user
             account.created_at = timezone.now()
+            account.balance = 0
             account.save()
-            return redirect("stocktracker:index")
+            # create asset_cash
+            asset_cash = AssetCash(
+                account_id=account,
+                created_at = timezone.now(),
+                updated_at = timezone.now(),
+                user_id = request.user,
+                balance=0
+            )
+            asset_cash.save()
+            return redirect("stocktracker:asset_cash_update")
     else:
         form = AccountForm(initial={'balance':0})
     return render(request, 'stocktracker/account_form.html', {'form':form})
+
+@login_required(login_url='common:login')
+def asset_cash_update(request):
+    """
+    update asset_cash
+    """
+    account_list =  Account.objects.filter(user_id=request.user)
+    if request.method == 'POST':
+        form = AssetCashForm(request.POST)
+        if form.is_valid():
+            account = Account.objects.get(id=int(request.POST['account_id']))
+            account.balance += int(request.POST['balance'])
+            account.save()
+            asset_cash = AssetCash.objects.get(account_id=int(request.POST['account_id']))
+            asset_cash.updated_at = timezone.now()
+            asset_cash.balance = int(request.POST['balance'])
+            asset_cash.save()
+            return redirect("stocktracker:index")
+    else:
+        form = AssetCash()
+    return render(request, 'stocktracker/asset_cash_form.html', {'form':form, 'account_list':account_list})
